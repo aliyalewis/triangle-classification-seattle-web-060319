fd89810064ea3870f3a955d61f9b149b887e09ff
github username <github email address>
2019-06-12 17:11:04 -0700

    unfinished. illegal?

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a == b && b == c
+      return :equilateral
+    elsif
+      a == b && b != c || a ==c && c != b || a == b || b ==c || a == c
+      return :isosceles
+    elsif
+      a != b && a != c && b != c
+      :scalene
+    else
+      if kind.class != Triangle
+        begin
+          raise TriangleError
+        rescue TriangleError => error
+          puts error.message
+        end
+     end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a triangle!"
+    end
+  end
+
+
 end
